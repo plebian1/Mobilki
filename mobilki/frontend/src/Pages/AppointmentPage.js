617a31f0c621e2f5363f42215009c7296c9f4144
Halszka Ciesielska <halszka99@interia.eu>
2021-11-25 15:00:52 +0100

    date time picker almost working

--- mobilki/frontend/src/Pages/AppointmentPage.js
+++ mobilki/frontend/src/Pages/AppointmentPage.js
@@ -1,6 +1,7 @@
-import DateFnsUtils from '@date-io/date-fns';
-import { DatePicker, MuiPickersUtilsProvider, TimePicker } from '@material-ui/pickers';
+import pl from "date-fns/locale/pl";
 import React, { useState } from "react";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 import { useHistory } from "react-router";
 import useFetch from "use-http";
 import Navbar from './Navbar';
@@ -8,9 +9,18 @@ import Navbar from './Navbar';
 const AppointmentPage = () =>  {
   
   const history = useHistory();
-  const [selectedDate, setSelectedDate] = useState(new Date());
-  const [selectedTime, setselectedTime] = useState(new Date(0, 0, 0, 8));
+
+  // const excludeTimes = [new Date(0,0,0).setHours(8,30,0), new Date(0,0,0).setHours(9,30,0)];
+  var now  = new Date();
+  now.setDate(now.getDate() + 1)
+  now.setHours(8);
+  now.setMinutes(0);
+  now.setSeconds(0);
+  
+  const [selectedDate, setSelectedDate] = useState(now);
+  const [selectedTime, setselectedTime] = useState(now);
   const [appointmentId, setAppointmentId] = useState();
+  const [excludeTimes, setExcludeTimes] = useState([]);
 
   const {post, response } = useFetch(
       'api/appointments/new_appointment',
@@ -24,11 +34,17 @@ const AppointmentPage = () =>  {
 
   const handleDateChange = (date) => {
     setSelectedDate(date);
-  };
-
-  const handleTimeChange = (date) => {
     setselectedTime(date);
   };
+
+
+  const filterDays = (date) => {
+    if (date.getDay() === 0 || date.getDay() === 6) {
+        return false;
+    } else {
+        return true;
+    }
+}
   
   const addAppointment = async () => {
 
@@ -47,25 +63,21 @@ const AppointmentPage = () =>  {
       <Navbar></Navbar>
       <div class="center">
         <h3>Wybierz termin wizyty</h3>
-        <MuiPickersUtilsProvider utils={DateFnsUtils}>
-          <div class="pt-24">
-            <DatePicker
-              label="Data wizyty"
-              value={selectedDate}
-              onChange={handleDateChange}
-              format="MM/dd/yyyy"
-              />
-          </div>
-          <div class="pt-24">
-            <TimePicker
-              label="Godzina wizyty"
-              value={selectedTime}
-              onChange={handleTimeChange}
-              minutesStep={15}
-              ampm={false}
-              />
-          </div>
-        </MuiPickersUtilsProvider>
+        <DatePicker
+            selected={selectedDate}
+            onChange={handleDateChange}
+            showTimeSelect
+            dateFormat="dd/MM/yyyy hh:mm"
+            locale={pl}
+            minDate={now}
+            minTime={new Date().setHours(8,0,0)}
+            maxTime={new Date().setHours(12,0,0)}
+            excludeTimes={excludeTimes}
+            timeIntervals={10}
+            timeFormat="hh:mm"
+            timeCaption="Godzina"
+            filterDate={filterDays}
+        />
         <div class="pt-24">
           <a class="btn btn-primary" href={"/appointment/details/" + appointmentId} role="button" onClick={addAppointment}>Potwierdź termin wizyty</a>
         </div>
